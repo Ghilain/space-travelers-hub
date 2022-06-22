eddd54da7affec0d2b2a4d797d9b9800ab20067d
Ghilain <ishimweghilain5050@gmail.com>
2022-06-22 11:19:54 +0200

    Update page name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/UI/Header';
-import Myprofile from './pages/myprofile';
+import Myprofile from './pages/Myprofile';
 import RocketPage from './pages/RocketPage';
 
 function App() {
