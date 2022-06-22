b3c2c729cdec8c226a758091fec219b91c94fa7a
Ghilain <ishimweghilain5050@gmail.com>
2022-06-22 11:09:41 +0200

    Create basic structure for Rockets - Setup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/UI/Header';
 import Myprofile from './pages/myprofile';
+import RocketPage from './pages/RocketPage';
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <div className="divider" />
       <Routes>
         <Route exact path="/profile" element={<Myprofile />} />
+        <Route exact path="/" element={<RocketPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/RocketPage.js
@@ -0,0 +1,9 @@
+import Rocket from '../components/UI/rockets/Rocket';
+
+const RocketPage = () => (
+  <ul className="rocket-detail">
+    <Rocket />
+  </ul>
+);
+
+export default RocketPage;
