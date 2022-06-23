10a163cfa2c47d0b2f05b7b2ce600bfe9ccb6e71
Ghilain <ishimweghilain5050@gmail.com>
2022-06-23 13:04:27 +0200

    fix linter error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/UI/Header';
 import Mission from './pages/Mission';
-import Myprofile from './pages/Myprofile';
+import Myprofile from './pages/myprofile';
 import RocketPage from './pages/RocketPage';
 
 function App() {
