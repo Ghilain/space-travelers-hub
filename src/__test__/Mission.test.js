b087b71ca3ff2810b11f240bff225b714a277696
bamsi <hibamsi@gmail.com>
2022-06-24 09:50:28 +0300

    add test for the mission

---
+++ src/__test__/Mission.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import Missions from '../pages/Mission';
+
+const mission = [
+  {
+    description: 'Description example',
+    id: 1,
+    name: 'First mission',
+    reserved: true,
+  },
+];
+
+describe('testing the rendering of missions', () => {
+  it('', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Missions key={mission[0]} mission={mission[0]} />
+      </Provider>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
